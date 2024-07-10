d444750a339a99c619e6b2bba82bd14626ffbc6e
Anya Aven <leafeon96@gmail.com>
2024-07-10 19:52:37 -0400

    Refactor maxDepth

--- trees/max-depth/maxDepth.ts
+++ trees/max-depth/maxDepth.ts
@@ -6,11 +6,9 @@ import { TreeNodeNum } from "../common/tree";
  **/
 
 function maxDepth(node: TreeNodeNum | null): number {
-  if(node === null) return 0; // TODO: how does it ever get called with null?
-  if(node.children.length === 0) return 1;
+  if(node === null) return 0;
 
   let highestPath = 1;
-  // let count = 1;
   for(const child of node.children){
     const pathDepth = maxDepth(child) + 1 // + 1 to count the node passed in
     if(pathDepth > highestPath) highestPath = pathDepth
