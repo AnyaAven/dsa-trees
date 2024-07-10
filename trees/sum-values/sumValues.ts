842d4f89a6c35637cf81dfaf2c9ccde6634ff568
Anya Aven <leafeon96@gmail.com>
2024-07-10 15:37:20 -0400

    Finish sumValues recuresively

--- trees/sum-values/sumValues.ts
+++ trees/sum-values/sumValues.ts
@@ -2,7 +2,7 @@ import { TreeNodeNum } from "../common/tree";
 
 /** sumValues(): add up all values of node and its descendants.
  * Returns sum as an integer. */
-function sumValues(node: TreeNodeNum): number {
+function sumValuesLoop(node: TreeNodeNum): number {
     let sum = 0;
 
     const toVisit = [node];
@@ -17,6 +17,15 @@ function sumValues(node: TreeNodeNum): number {
       }
     }
     return sum;
+}
+
+function sumValues(node: TreeNodeNum): number {
+  if(node.children.length === 0) return node.val;
+  let sum = node.val;
+  for(const child of node.children){
+    sum += sumValues(child)
+  }
+  return sum;
 }
 
 export { sumValues };
