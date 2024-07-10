abbf26ca59163b18e58c8e300d3b5a7de638315b
Anya Aven <leafeon96@gmail.com>
2024-07-10 19:00:09 -0400

    Finish maxDepth - Recursion

--- trees/max-depth/maxDepth.ts
+++ trees/max-depth/maxDepth.ts
@@ -6,7 +6,16 @@ import { TreeNodeNum } from "../common/tree";
  **/
 
 function maxDepth(node: TreeNodeNum | null): number {
-  return 42;
+  if(node === null) return 0; // TODO: how does it ever get called with null?
+  if(node.children.length === 0) return 1;
+
+  let highestPath = 1;
+  // let count = 1;
+  for(const child of node.children){
+    const pathDepth = maxDepth(child) + 1 // + 1 to count the node passed in
+    if(pathDepth > highestPath) highestPath = pathDepth
+  }
+  return highestPath;
 }
 
 export { maxDepth };
