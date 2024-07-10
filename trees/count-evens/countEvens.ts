fdc319e3f7198e7d441f19bab9dc87c870ee0ff9
Anya Aven <leafeon96@gmail.com>
2024-07-10 14:53:46 -0400

    Finish countEvens

--- trees/count-evens/countEvens.ts
+++ trees/count-evens/countEvens.ts
@@ -4,7 +4,20 @@ import { TreeNodeNum } from "../common/tree";
  * children, count how many nodes have even values. Returns that count as
  * an integer. */
 function countEvens(node: TreeNodeNum): number {
-  return 42;
+  let eventCount = 0;
+
+  const toVisit = [node];
+
+  while(toVisit.length > 0){
+    const current = toVisit.pop()!;
+
+    if(current.val % 2 === 0) eventCount++;
+
+    for(const child of current.children){
+      toVisit.push(child);
+    }
+  }
+  return eventCount;
 }
 
 export { countEvens };
