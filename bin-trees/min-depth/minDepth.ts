80ddac75f53c3993aaaf25f6a8e2bfb003aa2e86
Anya Aven <leafeon96@gmail.com>
2024-07-10 20:11:36 -0400

    finish minDepth - Recursive

--- bin-trees/min-depth/minDepth.ts
+++ bin-trees/min-depth/minDepth.ts
@@ -6,7 +6,23 @@ import { BNodeNum } from "../common/bintree";
  **/
 
 function minDepth(node: BNodeNum): number {
-  return 42;
+  if(node.lnode === null && node.rnode === null) return 1;
+
+  // If we only have the right side, only search right
+  if(node.lnode === null){
+    return minDepth(node.rnode!) + 1
+  }
+
+  // If we only have the left side, only search left
+  if(node.rnode === null){
+    return minDepth(node.lnode!) + 1
+  }
+
+  // If we have both nodes, determine which is the min.
+  const lDepth = minDepth(node.lnode);
+  const rDepth = minDepth(node.rnode);
+
+  return Math.min(lDepth, rDepth) + 1;
 }
 
 export { minDepth };
